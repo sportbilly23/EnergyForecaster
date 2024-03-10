acb7541d872401f3b47837fde9a0b2cfdf2ae119
sportbilly <sportbilly23@yahoo.com>
2024-03-10 11:05:35 +0200

    datacontroller correction

--- EnergyForecaster.py
+++ EnergyForecaster.py
@@ -43,6 +43,7 @@ if __name__ == '__main__':
     ef.data_controller.get_dataset('sb')
     sb = ef.data_controller.datasets['sb']
     sb2 = ef.data_controller.datasets['sb2']
+    ef.data_controller.import_csv('C:\\Users\\sportbilly\\Downloads\\consumption.csv', h5_name='test')
     # sb.to_timestamp('time', '%Y-%m-%d %H:%M:%S', assign='inplace', tzone='utc')
     # sb.minmax(column='temperature', assign='inplace')
     # sb.set_units('temperature', 'oC')
@@ -92,7 +93,7 @@ if __name__ == '__main__':
     # sb.lagged_series('weekday', name='weekday', lags=(0, 2, 3, 4, 5, 6, 7), assign='inplace')
     # sb.lagged_series('temperature', 'temp', lags=(1, 2), assign='add')
     # ef.process_controller.set_process('sb', lags=3, black_lags=2, target_length=24)
-    ef.process_controller.get_process('sb')
+    # ef.process_controller.get_process('sb')
     # ef.process_controller.insert_data('sb', ['temperature', 'cloud_cover',
     #                                          'irradiance_surface', 'precipitation'], no_lags=False)
     # ef.process_controller.insert_data('sb', ['time_204588272', 'time_588562846', 'time_728017024', 'time_675637917'])
@@ -124,6 +125,6 @@ if __name__ == '__main__':
     #                                             intervals_from_residuals=True, alpha=0.55)
     # axes = sb2.plot_seasonality('ES_load_actual_entsoe_transparency', 7*24, number_of_periods=3, trend_sign='div')
     # axes = sb2.plot_seasonality('ES_load_actual_entsoe_transparency', 7 * 24, number_of_periods=3, trend_sign='sub')
-    sb2.plot_classical_decomposition('ES_load_actual_entsoe_transparency', 168, number_of_periods=3, trend_sign='sub', seasonal_sign='sub')
+    # sb2.plot_classical_decomposition('ES_load_actual_entsoe_transparency', 168, number_of_periods=3, trend_sign='sub', seasonal_sign='sub')
     print(time.time())
 
